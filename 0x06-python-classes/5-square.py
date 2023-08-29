c76ecc06ebc38bebf8ccb614931edec77c97984e
Edgar Baluku <edgarbaluku@gmail.com>
2023-08-29 21:57:20 +0300

    square 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,29 +1,61 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
+    """
+    class square that has attributes:
+        size
+    some attributes are protected from input.
+    """
     def __init__(self, size=0):
-        self.__size = size
+        """
+        initialization function for our square clasee
+        """
+        if self.__validate_size(size):
+            self.__size = size
 
     @property
     def size(self):
+        """
+        getter for the size property
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+        """
+        setter for the size property
+        """
+        if self.__validate_size(value):
+            self.__size = value
 
     def area(self):
-        return self.__size * self.__size
+        """
+        calculates the area of the square
+        """
+        return self.__size ** 2
 
     def my_print(self):
-        if self.__size == 0:
-            print("")
+        """
+        prints the square using '#' characters
+        """
+        i = 0
+        for i in range(0, self.__size):
+            j = 0
+            for j in range(0, self.__size):
+                print("#", end='')
+            print()
+
+    def __validate_size(self, size):
+        """
+        validates the size, checking for errors
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
-            for iter in range(0, self.__size):
-                for iter2 in range(0, self.__size):
-                    print("#", end="")
-                print("")
+            return True
+        return False
 
