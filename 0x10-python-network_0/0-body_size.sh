d89e733cbe29ee23ca21b0fff558a1911078e1c6
Edgar Baluku <edgarbaluku@gmail.com>
2023-12-01 10:25:21 +0300

    d

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# send a request to an URL with curl, and displays the size of the body of the response
-# Check if URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+# takes in a URL, sends a request to that URL, displays the size of the body of the response
+curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
