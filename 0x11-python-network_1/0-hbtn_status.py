7beab021124b6d6f06092befcbd9a328d7889323
Edgar Baluku <edgarbaluku@gmail.com>
2023-12-03 10:08:40 +0300

    correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
+
+"""
+Fetching a url with urllib package
+"""
+
+
 import urllib.request
 
+# Assign the given url to a url variable
+url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+# Printing the response of the body
+print('Body response:')
+print('\t- type:', type(body))
+print('\t- content:', body)
+print('\t- utf8 content:', body.decode('utf-8'))
